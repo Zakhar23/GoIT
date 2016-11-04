4317e146473d0867d57f270cf30877bcd8ac0d43
kyrychenko <kyrychenko.zakhar@gmail.com>
2016-11-04 09:53:17 +0200

    w

---
+++ src/Module2/HomeWork/hw_2_1.java
@@ -0,0 +1,14 @@
+package Module2.HomeWork;
+
+/**
+ * Created by kyrychenko on 04.11.2016.
+ */
+public class hw_2_1 {
+    public static void main(String[] args) {
+
+        int[] MyArray = {10, 8, 47, 42, 3, -8, -45, 8, 1, 13};
+
+
+
+    }
+}
