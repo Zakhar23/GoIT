423264932c0b5c47101b47e0a109e6469c0ee5c1
Zakhar23 <kyrychenko.zakhar@gmail.com>
2016-11-06 17:54:20 +0200

    My first commit 3

---
+++ src/Module2/HomeWork/hw_2_2.java
@@ -0,0 +1,29 @@
+package Module2.HomeWork;
+
+/**
+ * Created by Home on 06.11.2016.
+ */
+public class hw_2_2 {
+
+    public static void main(String[] args) {
+
+        double balance = 1000.25;
+        double Cash = 1000.05;
+
+        GetCash(balance, Cash);
+
+    }
+
+
+    static void GetCash(double balance, double sum) {
+
+        double commissions = sum * 0.05;
+
+        if (sum + commissions < balance)
+            System.out.println("Успешно! Операция на сумму " + (sum + commissions) + " грн. Остаток: " + (balance - (sum + commissions)));
+        else
+            System.out.println("Операция отклонена, не достаточный баланс!");
+
+    }
+}
+
