f354a546df46eebb22ab4ee516edad92e452e461
Zakhar23 <kyrychenko.zakhar@gmail.com>
2017-01-26 13:23:49 +0200

    hw 8

--- src/main/java/module8/homework/UserDAO.java
+++ src/main/java/module8/homework/UserDAO.java
@@ -1,24 +1,6 @@
 package module8.homework;
 
-import java.util.List;
-
 public class UserDAO extends AbstractDAOImpl<User> {
-    public User save(User user) {
-        return (User) super.save(user);
-    }
-
-    public void delete(User user) {
-        super.delete(user);
-    }
-
-    public void deleteAll(List list) {
-        super.deleteAll(list);
-    }
-
-    public void saveAll(List list) {
-        super.saveAll(list);
-    }
-
     public void deleteById(long id) {
         listDB.removeIf(p -> p.getId() == id);
     }
