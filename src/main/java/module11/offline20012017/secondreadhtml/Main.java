cc3fc2b9d1fa51b60dcf509ed5e541b53d5ec1da
Zakhar23 <kyrychenko.zakhar@gmail.com>
2017-01-27 01:24:55 +0200

    for Travis

--- src/main/java/module11/offline20012017/secondreadhtml/Main.java
+++ src/main/java/module11/offline20012017/secondreadhtml/Main.java
@@ -1,6 +1,11 @@
-package module11.offline20012017.secondReadhtml;
+package module11.offline20012017.secondreadhtml;
 
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.OutputStreamWriter;
+import java.io.FileOutputStream;
+import java.io.InputStreamReader;
 import java.net.MalformedURLException;
 import java.net.URL;
 
@@ -15,7 +20,8 @@ public class Main {
         }
 
         BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(url.openStream()));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(new FileOutputStream("C:\\Users\\Home\\Desktop\\google.txt"),"UTF8"));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter
+                (new FileOutputStream("C:\\Users\\Home\\Desktop\\google.txt"), "UTF8"));
 
         String line;
 
@@ -25,7 +31,6 @@ public class Main {
             bw.newLine();
             System.out.println(line);
         }
-
 
 
     }
--- src/main/java/module9/offline13012017/qwe.java
+++
@@ -1,7 +0,0 @@
-package module9.offline13012017;
-
-/**
- * Created by Home on 13.01.2017.
- */
-public class qwe {
-}
