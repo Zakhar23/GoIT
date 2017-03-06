320824cddbaada5a69d49a57e0b2b5b0aa76c0ca
Zakhar23 <kyrychenko.zakhar@gmail.com>
2017-03-06 18:24:01 +0200

    ee hw 1, переделал структуру таблиц

--- src/main/java/developer/module1/homework/first/initDB.sql
+++ src/main/java/developer/module1/homework/first/initDB.sql
@@ -1,26 +1,7 @@
-CREATE TABLE developers (
-  ID   INT         NOT NULL,
-  NAME VARCHAR(30) NOT NULL,
-  PRIMARY KEY (ID)
-);
-
 CREATE TABLE skills (
-  ID_dev INT         NOT NULL,
-  skill  VARCHAR(30) NOT NULL,
-  FOREIGN KEY (ID_dev) REFERENCES developers (ID)
-);
-
-CREATE TABLE projects (
   ID   INT         NOT NULL,
   name VARCHAR(30) NOT NULL,
   PRIMARY KEY (ID)
-);
-
-CREATE TABLE developers_projects (
-  ID_dev     INT NOT NULL,
-  ID_project INT NOT NULL,
-  FOREIGN KEY (ID_dev) REFERENCES developers (ID),
-  FOREIGN KEY (ID_project) REFERENCES projects (ID)
 );
 
 CREATE TABLE companies (
@@ -29,11 +10,21 @@ CREATE TABLE companies (
   PRIMARY KEY (ID)
 );
 
-CREATE TABLE developers_companies (
-  ID_dev     INT NOT NULL,
-  ID_company INT NOT NULL,
+CREATE TABLE developers (
+  ID         INT         NOT NULL,
+  NAME       VARCHAR(30) NOT NULL,
+  project_id INT         NOT NULL,
+  company_id INT         NOT NULL,
+  PRIMARY KEY (ID),
+  FOREIGN KEY (project_id) REFERENCES skills (ID),
+  FOREIGN KEY (company_id) REFERENCES companies (ID)
+);
+
+CREATE TABLE developers_skills (
+  ID_dev   INT NOT NULL,
+  ID_skill INT NOT NULL,
   FOREIGN KEY (ID_dev) REFERENCES developers (ID),
-  FOREIGN KEY (ID_company) REFERENCES companies (ID)
+  FOREIGN KEY (ID_skill) REFERENCES skills (ID)
 );
 
 CREATE TABLE customers (
@@ -42,9 +33,35 @@ CREATE TABLE customers (
   PRIMARY KEY (ID)
 );
 
+CREATE TABLE projects (
+  ID          INT         NOT NULL,
+  name        VARCHAR(30) NOT NULL,
+  customer_id INT         NOT NULL,
+  PRIMARY KEY (ID),
+  FOREIGN KEY (customer_id) REFERENCES customers (ID)
+);
+
+/*
+CREATE TABLE developers_projects (
+  ID_dev     INT NOT NULL,
+  ID_project INT NOT NULL,
+  FOREIGN KEY (ID_dev) REFERENCES developers (ID),
+  FOREIGN KEY (ID_project) REFERENCES projects (ID)
+);
+
+CREATE TABLE developers_companies (
+  ID_dev     INT NOT NULL,
+  ID_company INT NOT NULL,
+  FOREIGN KEY (ID_dev) REFERENCES developers (ID),
+  FOREIGN KEY (ID_company) REFERENCES companies (ID)
+);
+*/
+
+/*
 CREATE TABLE customers_projects (
   ID_custom  INT NOT NULL,
   ID_project INT NOT NULL,
   FOREIGN KEY (ID_custom) REFERENCES customers (ID),
   FOREIGN KEY (ID_project) REFERENCES projects (ID)
 );
+*/
