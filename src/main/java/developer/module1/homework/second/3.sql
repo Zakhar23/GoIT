aec80bdad05e8ba2b576a4a554f8a0ca30ced848
Zakhar23 <kyrychenko.zakhar@gmail.com>
2017-03-10 00:20:06 +0200

    ee hw 1, переделал 3 и 5 задания

--- src/main/java/developer/module1/homework/second/3.sql
+++ src/main/java/developer/module1/homework/second/3.sql
@@ -1,2 +1,5 @@
-SELECT sum(salary) AS 'common salary'
-FROM developers;
+SELECT sum(developers.salary)
+FROM developers_skills
+  INNER JOIN skills ON developers_skills.ID_skill = skills.ID
+  INNER JOIN developers ON developers_skills.ID_dev = developers.ID
+WHERE skills.name = 'java';
--- src/main/java/developer/module1/homework/second/5.sql
+++ src/main/java/developer/module1/homework/second/5.sql
@@ -1,7 +1,19 @@
+CREATE TEMPORARY TABLE companies_projectCost
+  SELECT
+    companies.NAME     AS 'comp_name',
+    min(projects.cost) AS 'proj_cost'
+
+  FROM developers
+    LEFT JOIN companies ON developers.company_id = companies.ID
+    LEFT JOIN projects ON developers.project_id = projects.ID
+    LEFT JOIN customers ON projects.customer_id = customers.ID
+  GROUP BY companies.NAME;
+
 SELECT
-  customers.NAME     AS 'customer'
-FROM projects
-  INNER JOIN customers ON projects.customer_id = customers.ID
-GROUP BY customers.NAME
-ORDER BY SUM(projects.cost)
-LIMIT 1
+  cp.comp_name   AS 'Company',
+  cp.proj_cost   AS 'Cost',
+  customers.NAME AS 'Customer'
+FROM companies_projectCost cp
+  LEFT JOIN projects ON cp.proj_cost = projects.cost
+  LEFT JOIN customers ON projects.customer_id = customers.ID
+
